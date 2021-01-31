135580ba4ee047fffbb8c58cce0714083511a830
PierreSQS <nomo_messina@web.de>
2021-02-01 00:20:05 +0100

    Initial project setup for start

---
+++ src/test/java/pierrot/mvc/MvcContentNegViewsApplicationTests.java
@@ -0,0 +1,13 @@
+package pierrot.mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcContentNegViewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
